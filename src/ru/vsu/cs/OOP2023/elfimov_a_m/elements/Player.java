3724c1a031c568b48606c1a451dba695f29e55a7
Devonty <Devonty@yandex.ru>
2023-11-28 02:04:26 +0300

    Player interface added

--- src/ru/vsu/cs/OOP2023/elfimov_a_m/elements/Player.java
+++ src/ru/vsu/cs/OOP2023/elfimov_a_m/elements/Player.java
@@ -1,63 +1,19 @@
 package ru.vsu.cs.OOP2023.elfimov_a_m.elements;
 
-import ru.vsu.cs.OOP2023.elfimov_a_m.utils.PrintUtils;
+import ru.vsu.cs.OOP2023.elfimov_a_m.utils.TurnRecord;
 
-import java.util.*;
-
-public class Player {
-    public static final int MAX_CARD_ON_HAND = 6;
-    protected final List<Card> cardsOnHand;
-    public final String name;
-
-    public Player(String name) {
-        this.name = name;
-        this.cardsOnHand = new LinkedList<>();
-    }
-
-    public void addCard(Card cardToAdd){
-        cardsOnHand.add(cardToAdd);
-        sortCardsOnHand();
-    }
-
-    public boolean needCard(){
-        return cardsOnHand.size() < MAX_CARD_ON_HAND;
-    }
-
-    private void sortCardsOnHand() {
-        cardsOnHand.sort(new Comparator<Card>() {
-            @Override
-            public int compare(Card c1, Card c2) {
-                String s1 = cardToStringForCompare(c1);
-                String s2 = cardToStringForCompare(c2);
-                return s1.compareTo(s2);
-            }
-
-            private String cardToStringForCompare(Card card){
-                return (card.isTrump() ? "1" : "0") + card.getSuit() + card.getValue();
-            }
-        });
-    }
-
-    public Card peekCardAt(int index){
-        // will return card at index
-        assert 0 <= index && index < cardsOnHand.size() : "Индекс за границей: " + index+ " of " + cardsOnHand.size();
-        return cardsOnHand.get(index);
-    }
-    public Card takeCardAt(int index){
-        // will return and delete card at index
-        assert 0 <= index && index < cardsOnHand.size() : "Индекс за границей: " + index + " of " + cardsOnHand.size();
-        return cardsOnHand.remove(index);
-    }
+public interface Player {
+    void addCard(Card cardToAdd);
+    boolean needCard();
 
 
-    public void printCardOnHand(){
-        PrintUtils.printCardsInLine(cardsOnHand);
-        PrintUtils.printNumeric(cardsOnHand.size());
-    }
+    Card peekCardAt(int index);
 
-    public int countCardsOnHand(){
-        return cardsOnHand.size();
-    }
+    Card takeCardAt(int index);
 
+    String getName();
+
+    void printCardOnHand();
+    int countCardsOnHand();
 
 }
